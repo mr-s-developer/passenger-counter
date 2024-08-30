502ccfaca91909ce3ec63664db3efd4ae23bf9df
Sergey Sokolov <sergey.sokolov.ssa@gmail.com>
2024-08-30 18:16:36 +0500

    FIX: js script

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+(function() {
 
-function increment() {
-  count += 1
-  countEl.textContent = count
-}
+  let saveEl = document.getElementById("save-el")
+  let countEl = document.getElementById("count-el")
+  let count = 0
 
-function save() {
-  let countStr = count + " - "
-  saveEl.textContent += countStr
-  countEl.textContent = 0
-  count = 0
-}
+  function increment() {
+    count += 1
+    countEl.textContent = count
+  }
 
+  function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+  }
+
+})();
